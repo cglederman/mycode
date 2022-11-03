32e1ac0a91e2a6655e08a72d2d19b9cb45ff54b5
cglederman <cg_lederman@yahoo.com>
2022-11-03 20:20:35 +0000

    learning about http post

---
+++ httppost01.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+    "encoding/json"
+    "fmt"
+    "log"
+    "net/http"
+    "net/url"
+)
+
+func main() {
+    data := url.Values{
+        "name":  {"John Doe"},
+        "occupation": {"gardener"},
+    }
+
+    resp, err := http.PostForm("https://httpbin.org/post", data)
+
+     if err != nil {
+        log.Fatal(err)
+    }
+
+    var res map[string]any
+
+    json.NewDecoder(resp.Body).Decode(&res)
+
+    fmt.Println(res["form"])
+}
