d2ec99b4602d227fb2be449036cdccd3c18aa99d
cglederman <cg_lederman@yahoo.com>
2022-11-03 20:46:17 +0000

    learning about http servers

---
+++ httpsvr01.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+func main() {
+    http.HandleFunc("/", HelloHandler)
+    fmt.Println("Server started at port 8089")
+    log.Fatal(http.ListenAndServe(":8089", nil))      // listens on TCP network address for incoming HTTP requests
+}
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, there\n")
+}
+
